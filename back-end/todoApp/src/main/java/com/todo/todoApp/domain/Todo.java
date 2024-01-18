57a0d756349d4b7bc8d9f447b6503ed12e08fba0
changyeol oh <cy950315@naver.com>
2024-01-18 23:07:50 +0900

    중간 커밋

--- back-end/todoApp/src/main/java/com/todo/todoApp/domain/Todo.java
+++ back-end/todoApp/src/main/java/com/todo/todoApp/domain/Todo.java
@@ -1,11 +1,23 @@
-package com.todo.todoApp.entity;
+package com.todo.todoApp.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Todo {
+
     @Id
-    Long id;
-    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
